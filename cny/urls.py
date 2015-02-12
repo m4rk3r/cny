bc4199c1585c8ec06b1e3f11c85759d1e1716645
m4rk3r <mbeasl1@gmail.com>
2015-02-12 01:12:30 -0600

    New url :

--- cny/urls.py
+++ cny/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    (r'^$','rsvp.views.home'),
+    (r'^gung-hay-fat-choy/$','rsvp.views.home'),
     url(r'^admin/', include(admin.site.urls)),
 )
